79802b278b1d28265330639a9a799081a1d6a92d
Gabriel Augusto <biga00145@gmail.com>
2021-07-11 19:24:05 -0300

    Commit Inicial

---
+++ src/main/java/br/com/projeto/kafka/java/ProjetoKafkaJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.kafka.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoKafkaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoKafkaJavaApplication.class, args);
+	}
+
+}
